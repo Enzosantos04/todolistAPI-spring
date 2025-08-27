bb66ce17b1270f38b9f4f843df50472d5af1c4a8
enzosdev <enzohfs@gmail.com>
2025-08-27 12:38:33 -0300

    feat: Task entity

--- src/main/java/dev/java/todolist/entity/Task.java
+++ src/main/java/dev/java/todolist/entity/Task.java
@@ -1,4 +1,30 @@
 package dev.java.todolist.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.CurrentTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Table(name = "task")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    @CreationTimestamp
+    private LocalDate createdAt;
+    @UpdateTimestamp
+    private LocalDate updatedAt;
 }
