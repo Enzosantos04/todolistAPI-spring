93293e9f3f50578c45c042eded8c2bbe2f69077f
enzosdev <enzohfs@gmail.com>
2025-08-28 10:57:05 -0300

    feat: Mapper for TaskDTO

--- src/main/java/dev/java/todolist/mapper/TaskMapper.java
+++ src/main/java/dev/java/todolist/mapper/TaskMapper.java
@@ -1,7 +1,29 @@
 package dev.java.todolist.mapper;
 
+import dev.java.todolist.dto.TaskDTO;
+import dev.java.todolist.entity.Task;
 import org.springframework.stereotype.Component;
 
 @Component
-public class mapper {
+public class TaskMapper {
+
+    public Task map(TaskDTO taskDTO) {
+        Task newTask = new Task();
+        newTask.setId(taskDTO.getId());
+        newTask.setTitle(taskDTO.getTitle());
+        newTask.setDescription(taskDTO.getDescription());
+        newTask.setStatus(taskDTO.getStatus());
+        return newTask;
+
+
+    }
+
+    public TaskDTO map(Task task){
+        TaskDTO taskDTO = new TaskDTO();
+        taskDTO.setId(task.getId());
+        taskDTO.setTitle(task.getTitle());
+        taskDTO.setDescription(task.getDescription());
+        taskDTO.setStatus(task.getStatus());
+        return taskDTO;
+    }
 }
