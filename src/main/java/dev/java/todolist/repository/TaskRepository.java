002ad85422fdf85acee6d40d3450de0656792314
enzosdev <enzohfs@gmail.com>
2025-08-28 19:22:02 -0300

    feat: Add derived query on TaskRepository for findTaskByTitle

--- src/main/java/dev/java/todolist/repository/TaskRepository.java
+++ src/main/java/dev/java/todolist/repository/TaskRepository.java
@@ -4,4 +4,5 @@ import dev.java.todolist.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    Task findTaskByTitle(String title);
 }
