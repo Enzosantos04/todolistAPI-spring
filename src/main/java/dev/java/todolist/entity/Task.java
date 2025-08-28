ae35cfb7d078d5b8ba0ee7a26ae490d364ab907b
enzosdev <enzohfs@gmail.com>
2025-08-28 10:57:44 -0300

    style: removes whitespace

--- src/main/java/dev/java/todolist/entity/Task.java
+++ src/main/java/dev/java/todolist/entity/Task.java
@@ -6,10 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.CurrentTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
