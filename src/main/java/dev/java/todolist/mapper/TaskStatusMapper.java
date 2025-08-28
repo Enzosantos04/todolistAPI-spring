9c457647e5a2723d0e513fd2e306120418702b14
enzosdev <enzohfs@gmail.com>
2025-08-28 20:08:05 -0300

    feat: Mapper for TaskStatusDTO

--- src/main/java/dev/java/todolist/mapper/TaskStatusMapper.java
+++ src/main/java/dev/java/todolist/mapper/TaskStatusMapper.java
@@ -1,4 +1,17 @@
 package dev.java.todolist.mapper;
 
+import dev.java.todolist.dto.TaskStatusDTO;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TaskStatusMapper {
+    public TaskStatusDTO map(String status) {
+        TaskStatusDTO taskStatusDTO = new TaskStatusDTO();
+        taskStatusDTO.setStatus(status);
+        return taskStatusDTO;
+    }
+
+    public String map(TaskStatusDTO taskStatusDTO) {
+        return taskStatusDTO.getStatus();
+    }
 }
