65a9695c3110735ce1ab385d0409b623209cda66
enzosdev <enzohfs@gmail.com>
2025-08-28 11:43:22 -0300

    feat: Adding business rule, at the moment ive implemented only listAllTask and createTask

--- src/main/java/dev/java/todolist/service/TaskService.java
+++ src/main/java/dev/java/todolist/service/TaskService.java
@@ -1,7 +1,37 @@
 package dev.java.todolist.service;
 
+import dev.java.todolist.dto.TaskDTO;
+import dev.java.todolist.entity.Task;
+import dev.java.todolist.mapper.TaskMapper;
+import dev.java.todolist.repository.TaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    private final TaskMapper taskMapper;
+    public TaskService(TaskRepository taskRepository, TaskMapper taskMapper) {
+        this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
+    }
+
+
+    public List<TaskDTO> listAllTask(){
+        List<Task> tasks = taskRepository.findAll();
+        return tasks.stream()
+                .map(taskMapper::map)
+                .collect(Collectors.toList());
+    }
+
+    public TaskDTO createTask(TaskDTO taskDTO){
+        Task task = taskMapper.map(taskDTO);
+        Task savedTask = taskRepository.save(task);
+        return taskMapper.map(savedTask);
+    }
 }
