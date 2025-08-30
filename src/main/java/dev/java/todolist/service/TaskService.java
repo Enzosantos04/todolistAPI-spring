41a43c02b15af24f8f0ca507c50424a0979d8e50
enzosdev <enzohfs@gmail.com>
2025-08-30 12:03:11 -0300

    style: remove white space

--- src/main/java/dev/java/todolist/service/TaskService.java
+++ src/main/java/dev/java/todolist/service/TaskService.java
@@ -7,7 +7,6 @@ import dev.java.todolist.mapper.TaskMapper;
 import dev.java.todolist.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
